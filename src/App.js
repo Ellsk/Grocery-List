26a749dedc3a762dff378c36f6b34811aca535e8
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2024-04-04 18:56:42 +0100

    Updated the Groceries list

--- src/App.js
+++ src/App.js
@@ -26,25 +26,47 @@ function App() {
     }
   ])
 
+  const [newItem, setNewItems] = useState('');
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  }
+  
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem  = { id, checked:false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
+
   const handleCheck = (id) => {
     // console.log(`key: ${id}`)
-    const listItems = items.map((item) => item.id === id ? {...item, 
-    checked: !item.checked} : item);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems))
+    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
+    setAndSaveItems(listItems);
   }
 
   const handleDelete = (id) => {
     // console.log(id);
      const listItems = items.filter((item) => item.id !== id)
-     setItems(listItems);
-     localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+     setAndSaveItems(listItems);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!newItem) return;
+    // console.log(`form submitted Item: ${newItem}`);
+    addItem(newItem);
+    setNewItems("");
+  }
   return (
     <div className="App">
        <Header title ="Grocery List"/>
-       <AddItem />
+       <AddItem 
+          newItem={newItem}
+          setNewItems={setNewItems}
+          handleSubmit={handleSubmit}
+       />
        <Content 
           items={items}
           handleCheck={handleCheck}
--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer({ length }) {
     // const today = new Date();
   return (
     <footer>
-       <p> {length} List {length == 1 ? ("item"):("items") }</p>
+       <p> {length} List {length === 1 ? ("item"):("items") }</p>
         {/* <p> Copyright &copy; {today.getFullYear()} </p> */}
     </footer>
   )
